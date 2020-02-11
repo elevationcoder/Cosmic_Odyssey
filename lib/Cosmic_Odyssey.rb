486d25845ceb9d022337fe9bf8a2cd48f09c58e6
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-11 21:36:27 +0000

    Updated CLI and env. file

--- lib/Cosmic_Odyssey.rb
+++ lib/Cosmic_Odyssey.rb
@@ -1,5 +1,5 @@
-require "Cosmic_Odyssey/version"
-
+require_relative "./Cosmic_Odyssey/version"
+require_relative "./Cosmic_Odyssey/CLI"
 module CosmicOdyssey
   class Error < StandardError; end
   # Your code goes here...
